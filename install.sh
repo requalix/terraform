bce1b31289a8aa0b071446a9302952f52fc47f7f
Jack Murray <jack@murray.cx>
2013-05-10 04:34:41 +0000

    added bin

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 x=$(pwd)
 for i in .*; do
-  [ $i != . -a $i != .. -a '!' -e ~/$i ] && ln -s "$x/$i" ~/$i
+  [ $i != . -a $i != .. -a $i != .git -a '!' -e ~/$i ] && ln -s "$x/$i" ~/$i
 done
 
 true

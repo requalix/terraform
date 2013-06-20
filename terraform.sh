caaace83a2c87a6f310c81229d10f8af46f8bddb
Jack Murray <jack@murray.cx>
2013-06-20 04:09:38 +0000

    everything

--- terraform.sh
+++ terraform.sh
@@ -4,11 +4,9 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 x=$(pwd)
 for i in .*; do
-  [ $i != . -a $i != .. -a $i != .git -a '!' -e ~/$i ] && ln -s "$x/$i" ~/$i
+  [ $i != . -a $i != .. -a $i != .git -a '!' -e ~/$i ] && ln "$@" -s "$x/$i" ~/$i
 done
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 vim +BundleInstall +qall
-
-true

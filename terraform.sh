bc100d9ddf68012da8f8e6daa6e67919e78234a6
Jack <jack@murray.cx>
2013-07-22 03:30:51 +1000

    added chrome and gnome terminal settings

--- terraform.sh
+++ terraform.sh
@@ -13,4 +13,19 @@ done
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
+settings="
+[legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9]
+background-color='rgb(0,0,0)'
+scrollbar-policy='never'
+use-theme-colors=false
+foreground-color='rgb(255,255,255)'
+use-system-font=false
+font='Monospace 8'
+
+[legacy]
+schema-version=uint32 3
+default-show-menubar=false"
+
+echo "$settings" | dconf load /org/gnome/terminal/
+
 vim +BundleInstall +qall
